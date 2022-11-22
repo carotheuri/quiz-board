b1ca454e6c4a8b80987696a3c55356d2ce3e86d1
carotheuri <princesstheush2@gmail.com>
2022-11-22 14:48:59 +0300

    adding the business logic for grading the exam

--- js/script.js
+++ js/script.js
@@ -3,8 +3,24 @@ $(function(){
         var fullName = $("#fullName").val();
         alert(fullName);
         var input = $("form").serializeArray();
-        console.log(input);
+        var result = gradeExam(input);
         event.preventDefault();
     });
 
 });
+
+function gradeExam(formElements){
+    alert("Well received");
+    console.log(formElements);
+    var correctAnswers = ['JavaScript is a scripting language used to make the website interactive','Yes, perfectly','All of the mentioned','Function/Method'];
+    var count = 0;
+    var output = '';
+    formElements.forEach(formElement => {
+        console.log(formElement['value']);
+        if(correctAnswers.indexOf(formElement['value'])){
+            count += 1;
+        }
+    });
+    // each mark is 25%
+    return (25*count);
+}
